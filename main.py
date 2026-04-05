ba0a2ddee6448d79cb9fa1e59d06993a39f5da8f
Mitul Khemani <mitulkhemani@gmail.com>
2026-04-05 19:37:07 +0530

    scheduler added

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 import json
 from pydantic import BaseModel
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = FastAPI()
+scheduler = BackgroundScheduler()
+scheduler.start()
 
 class PriceData(BaseModel):
     A: int
@@ -36,6 +39,7 @@ def add_price_schedule(data: PriceSchedule):
     schedules.append(data.dict())
 
     write_json("price_schedules.json", schedules)
+    schedule_jobs()
     return {"message": "saved", "data": schedules}
 
 @app.get("/price/schedules")
@@ -45,8 +49,52 @@ def get_schedules():
 @app.post("/audit/time")
 def set_audit(data: AuditConfig):
     write_json("audit_config.json", data.dict())
+    schedule_jobs()
     return {"message": "audit updated"}
 
 @app.get("/audit/time")
 def get_audit():
     return read_json("audit_config.json")
+
+#Scheduled Functions
+def run_price_update(prices):
+    print("Running price update:", prices)
+
+def run_audit():
+    print("Running night audit")
+
+def schedule_jobs():
+    scheduler.remove_all_jobs()
+
+    # Load price schedules
+    schedules = read_json("price_schedules.json")
+
+    for job in schedules:
+        time = job["time"]
+        h, m = map(int, time.split(":"))
+
+        scheduler.add_job(
+            run_price_update,
+            'cron',
+            hour=h,
+            minute=m,
+            args=[job["prices"]],
+            id=f"price_{time}",
+            replace_existing=True
+        )
+
+    # Load audit time
+    audit = read_json("audit_config.json")
+    h, m = map(int, audit["time"].split(":"))
+    scheduler.add_job(
+        run_audit,
+        'cron',
+        hour=h,
+        minute=m,
+        id="audit_job",
+        replace_existing=True
+    )
+
+@app.on_event("startup")
+def startup_event():
+    schedule_jobs()
