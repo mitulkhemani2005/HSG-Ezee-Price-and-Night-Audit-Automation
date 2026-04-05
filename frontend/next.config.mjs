a9c0d15b6d8c3e7263a510ae68aefb1c5e289a4d
Mitul Khemani <mitulkhemani@gmail.com>
2026-04-06 04:42:30 +0530

    complete deploy - 2

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -6,6 +6,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://98.88.83.55:8000/:path*', // Proxy to Backend
+      },
+    ]
+  },
 }
 
 export default nextConfig
